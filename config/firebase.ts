20699cd413f892b9c9897a4c764e77760df6b1d9
Megharaj Dandgavhal <megharaj.dandgavhal@tekditechnologies.com>
2025-09-24 16:42:00 +0530

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,15 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBeqU37f2wdYLjh0IiqeO9-VxZQ3WNtrqc",
-  authDomain: "appointment-scheduler-a215a.firebaseapp.com",
-  projectId: "appointment-scheduler-a215a",
-  storageBucket: "appointment-scheduler-a215a.firebasestorage.app",
-  messagingSenderId: "691602579895",
-  appId: "1:691602579895:web:c5a85e7f1988b2f2c08832",
-  measurementId: "G-N4WLWB88C9"
-};
+EXPO_PUBLIC_FIREBASE_API_KEY=AIzaSy*************
+EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN=appointment-scheduler-a215a.firebaseapp.com
+EXPO_PUBLIC_FIREBASE_PROJECT_ID=appointment-scheduler-a215a
+EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET=appointment-scheduler-a215a.firebasestorage.app
+EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=691602579895
+EXPO_PUBLIC_FIREBASE_APP_ID=1:691602579895:web:c5a85e7f1988b2f2c08832
+EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID=G-N4WLWB88C9
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
